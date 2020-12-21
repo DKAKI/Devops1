8b09773919a4be02c2b710ffbcc74b44b485a94e
DKAKI <50663884+DKAKI@users.noreply.github.com>
2020-12-21 23:34:03 +0100

    Update test.java

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;  
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;  
-import org.openqa.selenium.chrome.ChromeDriver;  
+import org.openqa.selenium.edge.EdgeDriver;  
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
   
@@ -10,17 +10,18 @@ public class example {
     public static void main(String[] args) {  
         
     // declaration and instantiation of objects/variables  
-    System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");  
-    WebDriver driver=new ChromeDriver();  
+    System.setProperty("webdriver.edge.driver", "D:\\msegdedriver.exe");  
+    WebDriver driver=new EdgeDriver();  
       
 // Launch website  
-    driver.navigate().to("http://localhost:9070/jenkins_calci");  
-          
-    
-          
-    // Click on the male button  
-    driver.findElement(By.id("txt1")).click(); 
-     // you can do as many types of tests this is just a basic one  
+    String url ="http://localhost:8070/";  
+    driver.get(url) ;
+    driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+    Screenshot s=new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+    ImageIO.write(s.getImage(),"PNG",new File("C:\\Users\\HP\\Desktop\\test\\test.png"));
+    driver.quit();
+
+
     }      
 
       
